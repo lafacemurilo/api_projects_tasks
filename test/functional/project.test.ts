9ed8205adec9c05f2bf6b4508b83254d3f98d301
murilo <murilo.laface@cactvs.com.br>
2021-10-04 14:21:01 -0300

    implementado teste para cadastro do projeto

--- test/functional/project.test.ts
+++ test/functional/project.test.ts
@@ -3,13 +3,30 @@ import { Projects } from '@src/controllers/projectController';
 describe('Beach project functional tests', () => {
   it('should return a project with just a few times', async () => {
     const { body, status } = await global.testRequest.get('/projects');
-
-    const response: Projects[] = [
-      { id: '1', title: 'Novo projeto', tasks: [] },
-      { id: '2', title: 'Novo projeto', tasks: [] },
-    ];
-
-    expect(body).toEqual(response);
     expect(status).toBe(201);
+  });
+});
+
+describe('When creating a new project', () => {
+  it('should successfully create a new project ', async () => {
+    const newProject: Omit<Projects, 'tasks'> = {
+      id: '1',
+      title: 'Novo projeto',
+    };
+
+    const response = await global.testRequest
+      .post('/projects')
+      .send(newProject);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual(
+      expect.arrayContaining([
+        {
+          id: expect.any(String),
+          title: expect.any(String),
+          tasks: expect.any(Array),
+        },
+      ])
+    );
   });
 });
