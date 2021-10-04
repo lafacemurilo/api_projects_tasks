56ad53a5befddc2112c0935a948737b15b378f1a
murilo <murilo.laface@cactvs.com.br>
2021-10-04 14:19:10 -0300

    add nova rota para cadastrar um novo projeto

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import {ProjectController} from '../controllers/projectController';
+import { ProjectController } from '../controllers/projectController';
 const router = express.Router();
 
 //objects
 const project = new ProjectController();
 
 //routes
-router.get("/projects", project.getProjects);
+router.get('/projects', project.getProjects);
+router.post('/projects', project.setProjects);
 
-export =  router;
+export = router;
