cfe30c62f97b2b5f1a5b43a619b145eef9848dbd
murilo <murilo.laface@cactvs.com.br>
2021-10-06 02:22:23 -0300

    iniciando arquivo teste para middlewate de validaçao do id

--- src/app.ts
+++ src/app.ts
@@ -7,16 +7,12 @@ let countReq = 0;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-
   countReq++;
-  console.log(`[${req.method}] ${req.url} || Quantidade de requisições: ${countReq}`);
+  console.log(
+    `[${req.method}] ${req.url} || Quantidade de requisições: ${countReq}`
+  );
   return next();
 });
-
-//connection database
-/*(async function () {
-  await database.connect();
-})();*/
 
 //routes
 app.use('/', projects);
