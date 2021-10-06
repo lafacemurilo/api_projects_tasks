4ed6d08daa1a5a6cf6c95ab993b9faaff8826a56
murilo <murilo.laface@cactvs.com.br>
2021-10-06 00:16:29 -0300

    iniciando o database no server para melhorias de testes

--- bin/server.ts
+++ bin/server.ts
@@ -1,7 +1,12 @@
 import app from '../src/app';
+import * as database from '../src/database';
 
 require('dotenv/config');
 const port = process.env.PORT || '3000';
+
+(async function () {
+  await database.connect();
+})();
 
 app.listen(port, function () {
   console.log(`app listenning on port ${port}`);
